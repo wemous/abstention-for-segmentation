c17cb3d54bd99f18d3795ab7245a340c3c5d93dc
Wesam Moustafa <wesam.moustafa@iais.fraunhofer.de>
2024-05-15 10:57:09 +0200

    base model

---
+++ models/base.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from importlib import import_module
+
+from lightning import LightningModule
+from torch import Tensor
+
+
+class BaseModel(LightningModule, ABC):
+    def __init__(self, num_classes: int, loss: dict, optimizer: dict, **kwargs):
+        super().__init__(**kwargs)
+        self.num_classes = num_classes
+        self.loss_function = getattr(import_module(loss["module"]), loss["name"])(
+            **loss["args"]
+        )
+        self.optimizer = getattr(import_module(optimizer["module"]), optimizer["name"])
+        self.optimizer_args = optimizer["args"]
+
+    @abstractmethod
+    def forward(self, x: Tensor, *args, **kwargs) -> Tensor: ...
+
+    def training_step(self, batch, batch_idx) -> Tensor:
+        images = batch[0].to(self.device)
+        targets = batch[1].to(self.device)
+        preds = self.forward(images)
+        loss = self.loss_function(preds, targets)
+        self.log(
+            "training loss",
+            loss,
+            prog_bar=True,
+            on_epoch=True,
+            sync_dist=True,
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx) -> Tensor:
+        images = batch[0].to(self.device)
+        targets = batch[1].to(self.device)
+        preds = self.forward(images)
+        loss = self.loss_function(preds, targets)
+        self.log(
+            "validation loss",
+            loss,
+            prog_bar=True,
+            on_epoch=True,
+            sync_dist=True,
+        )
+        return loss
+
+    def configure_optimizers(self):
+        return self.optimizer(self.parameters(), **self.optimizer_args)
--- models/unet.py
+++ models/unet.py
@@ -1,19 +1,18 @@
-import lightning as pl
 from mmseg.models.backbones.unet import InterpConv
 from mmseg.models.backbones.unet import UNet as U
 from torch import Tensor
-from torch.nn import Conv2d, CrossEntropyLoss
-from torch.optim.adam import Adam
+from torch.nn import Conv2d
 from torchvision.transforms.v2.functional import resize
 
+from models.base import BaseModel
 
-class UNet(pl.LightningModule):
-    def __init__(self, num_classes: int, **kwargs):
-        super().__init__()
+
+class UNet(BaseModel):
+    def __init__(self, num_classes: int, loss: dict, optimizer: dict, **kwargs):
+        super().__init__(num_classes, loss, optimizer, **kwargs)
         upsample_cfg = {"type": InterpConv}
         self.unet = U(upsample_cfg=upsample_cfg, **kwargs)
         self.out_conv = Conv2d(self.unet.base_channels, num_classes, kernel_size=1)
-        self.metric = CrossEntropyLoss().to(self.device)
 
     def forward(self, x: Tensor) -> Tensor:
         h, w = x.shape[-2:]
@@ -31,22 +30,3 @@ class UNet(pl.LightningModule):
             out = self.out_conv(out)
             out = resize(out, [h, w], antialias=True)
         return out
-
-    def training_step(self, batch, batch_idx) -> Tensor:
-        images = batch[0].to(self.device)
-        targets = batch[1].to(self.device)
-        preds = self.forward(images)
-        loss = self.metric(preds, targets)
-        self.log("training loss", loss, prog_bar=True, on_epoch=True, on_step=True)
-        return loss
-
-    def validation_step(self, batch, batch_idx) -> Tensor:
-        images = batch[0].to(self.device)
-        targets = batch[1].to(self.device)
-        preds = self.forward(images)
-        loss = self.metric(preds, targets)
-        self.log("validation loss", loss, prog_bar=True, on_epoch=True, on_step=True)
-        return loss
-
-    def configure_optimizers(self):
-        return Adam(self.parameters(), lr=3e-4)
