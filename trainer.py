bb2a1df4a516ae21c9fc804e025f7b9bdaa3f384
ppriya <priya.priya@iais.fraunhofer.de>
2024-05-28 23:09:14 +0200

    added sweep script

--- trainer.py
+++ trainer.py
@@ -1,76 +1,92 @@
 from importlib import import_module
-from random import seed
 
 import lightning as pl
 import torch
-import wandb
 from lightning.pytorch.loggers import WandbLogger
 from torch.utils.data import DataLoader
 from yaml import full_load
 
+import wandb
 from datasets import ADE20K, CaDIS
 
 config = full_load(open("train_config.yaml"))
-
-seed(config["seed"])
-torch.manual_seed(config["seed"])
-torch.cuda.manual_seed_all(config["seed"])
+pl.seed_everything(config["seed"])
 torch.set_float32_matmul_precision("high")
 
 dataset_name = config["dataset"]
 image_size = config["image_size"]
 transforms = config["transforms"]
 batch_size = config["batch_size"]
+setup = config["setup"]
 
 if dataset_name == "ade20k":
-    train_dataset = ADE20K(train=True, image_size=image_size)
-    valid_dataset = ADE20K(train=False, image_size=image_size)
-    num_classes = 151
+    train_dataset = ADE20K(split=0, setup=setup, image_size=image_size)
+    valid_dataset = ADE20K(split=1, setup=setup, image_size=image_size)
+    test_dataset = ADE20K(split=2, setup=setup, image_size=image_size)
 elif dataset_name == "cadis":
-    train_dataset = CaDIS(train=True, image_size=image_size, transforms=transforms)
-    valid_dataset = CaDIS(train=False, image_size=image_size)
-    num_classes = 8
+    train_dataset = CaDIS(
+        split=0,
+        setup=setup,
+        image_size=image_size,
+        transforms=transforms,
+    )
+    valid_dataset = CaDIS(split=1, setup=setup, image_size=image_size)
+    test_dataset = CaDIS(split=2, setup=setup, image_size=image_size)
 
-model_config = config["model"]
-module = model_config["module"]
-model_name = model_config["name"]
-model_args = model_config["args"]
-model = getattr(import_module(module), model_name)(num_classes, **model_args)
+num_classes = train_dataset.num_classes[setup]
 
 train_loader = DataLoader(
     train_dataset,
     batch_size=batch_size,
     shuffle=True,
     drop_last=False,
+    num_workers=8,
 )
 valid_loader = DataLoader(
     valid_dataset,
     batch_size=batch_size,
     shuffle=False,
     drop_last=False,
+    num_workers=8,
+)
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=8,
 )
 
-run_name = f"{model._get_name().lower()}-{dataset_name}-{model.loss_function._get_name()[:-4].lower()}"
+model_config = config["model"]
+module = model_config["module"]
+model_name = model_config["name"]
+model_args = model_config["args"]
+loss_config = model_args["loss"]
+optimizer_name = model_args["optimizer"]["name"]
 
-# logger = WandbLogger(
-#     name=run_name,
-#     project="DAC Segmentation",
-# )
+if loss_config["name"] == "DACLoss":
+    loss_config["args"]["max_epochs"] = config["max_epochs"]
+    num_classes += 1
+loss_config["args"]["num_classes"] = num_classes
 
-# wandb.login()
+model_args["loss"] = loss_config
+model = getattr(import_module(module), model_name)(num_classes, **model_args)
 
+run_name = f"{model_name.lower()}/{dataset_name}/{loss_config['name'][:-4].lower()}/{optimizer_name.lower()}"
+
+wandb.login()
+
+logger = WandbLogger(name=run_name, project="DAC Segmentation")
 trainer = pl.Trainer(
     devices=config["devices"],
     max_epochs=config["max_epochs"],
     enable_checkpointing=False,
     enable_model_summary=False,
     enable_progress_bar=True,
-    logger=False,
-    # logger=logger,
+    logger=logger,
 )
 
-
 trainer.fit(model, train_loader, valid_loader)
-# trainer.fit(model, valid_loader)
+trainer.test(model, test_loader)
 
-# wandb.finish()
+wandb.finish(quiet=True)
