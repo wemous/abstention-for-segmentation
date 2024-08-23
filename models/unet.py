9068aee6e6d49f11485c2c94c4b2331a38516a4d
wmoustafa <wesam.moustafa@iais.fraunhofer.de>
2024-08-23 18:54:57 +0200

    changed unet implementation

--- models/unet.py
+++ models/unet.py
@@ -1,37 +1,115 @@
-from mmseg.models.backbones.unet import InterpConv
-from mmseg.models.backbones.unet import UNet as U
-from torch import Tensor
-from torch.nn import Conv2d
-from torchvision.transforms.v2.functional import resize
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-from models.base import BaseModel
+from .base import BaseModel
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2), DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(
+                in_channels, in_channels // 2, kernel_size=2, stride=2
+            )
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
 
 
 class UNet(BaseModel):
-    def __init__(self, num_classes: int, loss: dict, optimizer: dict, **kwargs):
-        super().__init__(
-            num_classes=num_classes,
-            loss=loss,
-            optimizer=optimizer,
-            model_name="UNet",
-        )
-        upsample_cfg = {"type": InterpConv}
-        self.unet = U(upsample_cfg=upsample_cfg, **kwargs)
-        self.out_conv = Conv2d(self.unet.base_channels, num_classes, kernel_size=1)
+    def __init__(
+        self,
+        num_classes: int,
+        loss: dict,
+        optimizer: dict,
+        bilinear=False,
+    ):
+        super().__init__(num_classes, loss, optimizer, model_name="UNet")
+        self.n_channels = 3
+        self.bilinear = bilinear
 
-    def forward(self, x: Tensor) -> Tensor:
-        h, w = x.shape[-2:]
-        downsampling_factor = 2 ** (self.unet.num_stages - 1)
-        if h % downsampling_factor == 0 and w % downsampling_factor == 0:
-            out = self.unet(x)[-1]
-            out = self.out_conv(out)
-        else:
-            new_size = [
-                round(h / downsampling_factor) * downsampling_factor,
-                round(w / downsampling_factor) * downsampling_factor,
-            ]
-            x = resize(x, new_size, antialias=True)
-            out = self.unet(x)[-1]
-            out = self.out_conv(out)
-            out = resize(out, [h, w], antialias=True)
-        return out
+        self.inc = DoubleConv(3, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, num_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
