4bc921bd6a76ca801b226c36e66f90209198e426
wmoustafa <wesam.moustafa@iais.fraunhofer.de>
2024-11-14 00:36:24 +0100

    added asce loss

---
+++ asce_sweep.py
@@ -0,0 +1,91 @@
+import lightning as pl
+import torch
+from lightning.pytorch.loggers import WandbLogger
+from torch.utils.data import DataLoader
+
+import wandb
+from datasets import CaDIS, NoisyCaDIS, DSAD, NoisyDSAD
+from models import UNet, DeepLabV3Plus, FPN
+
+pl.seed_everything(13)
+torch.set_float32_matmul_precision("high")
+
+wandb.login()
+
+
+def main():
+    run = wandb.init()
+    config = wandb.config
+    max_epochs = 30
+
+    train_dataset = NoisyCaDIS(noise_level=3, setup=1)
+    valid_dataset = CaDIS(split="valid", setup=1)
+    test_dataset = CaDIS(split="test", setup=1)
+    num_classes = test_dataset.num_classes[1]
+    batch_size = 128
+
+    # train_dataset = NoisyDSAD(noise_level=3)
+    # valid_dataset = DSAD(split="valid")
+    # test_dataset = DSAD(split="test")
+    # num_classes = 8
+    # batch_size = 64
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=False,
+        num_workers=8,
+    )
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=8,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=8,
+    )
+
+    loss_config = {
+        "name": "ASCELoss",
+        "args": {
+            "noise_rate": round(train_dataset.noise_rate, 2),
+            "max_epochs": max_epochs,
+            "warmup_rate": 0.2,
+            "alpha": config.alpha,
+            "beta": config.beta,
+            "gamme": 1,
+        },
+    }
+    optimizer_args = {
+        "lr": config.lr,
+        "momentum": 0.9,
+        "weight_decay": 5e-3,
+    }
+
+    model = UNet(num_classes + 1, loss_config, **optimizer_args)
+    # model = DeepLabV3Plus(num_classes+1, loss_config)
+    # model = FPN(num_classes+1, loss_config)
+
+    trainer = pl.Trainer(
+        devices=1,
+        max_epochs=max_epochs,
+        enable_checkpointing=False,
+        enable_model_summary=False,
+        enable_progress_bar=True,
+        log_every_n_steps=len(train_loader) // 5,
+        logger=WandbLogger(id=run.id),
+    )
+
+    trainer.fit(model, train_loader, valid_loader)
+    trainer.test(model, test_loader)
+    wandb.finish(quiet=True)
+
+
+main()
--- losses.py
+++ losses.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 from torch import Tensor
 from torchvision.ops.focal_loss import sigmoid_focal_loss
 
@@ -21,17 +20,17 @@ class DACLoss(nn.Module):
     def __init__(
         self,
         max_epochs: int,
-        warmup_epochs: int = 0,
-        alpha_final=3.0,
+        warmup_rate: float = 0.2,
+        alpha_final=2.0,
         alpha_init_factor=64,
-        mu=0.2,
+        mu=0.05,
         **kwargs,
     ):
         super().__init__()
 
         # fixed values
         self.max_epochs = max_epochs
-        self.warmup_epochs = warmup_epochs if warmup_epochs > 0 else max_epochs // 5
+        self.warmup_epochs = round(max_epochs * warmup_rate)
         self.alpha_final = alpha_final
         self.alpha_init_factor = alpha_init_factor
         self.mu = mu
@@ -50,26 +49,26 @@ class DACLoss(nn.Module):
         training: bool = False,
         epoch: int = 0,
     ):
-        ce_loss = F.cross_entropy(preds[:, :-1, :, :], targets, reduction="none")
+        ce_loss = F.cross_entropy(preds[:, :-1, :, :], targets)
 
         if not training:
-            return ce_loss.mean()
+            return ce_loss
         else:
             abstention_rate = (
                 (preds.argmax(dim=1) == preds.shape[1] - 1).float().mean().item()
             )
-            abstention = torch.exp(F.log_softmax(preds, dim=1))[:, -1, :, :]
+            abstention = torch.exp(F.log_softmax(preds, dim=1))[:, -1, :, :].mean()
             abstention = torch.min(
                 abstention,
                 torch.tensor(
-                    [1 - self.epsilon],
+                    1 - self.epsilon,
                     device=abstention.device,
                 ),
             )
-            regularization = torch.tensor(0.0)
+            regularization = 0
 
             if epoch < self.warmup_epochs:
-                alpha_threshold = ((1 - abstention) * ce_loss).mean().item()
+                alpha_threshold = ((1 - abstention) * ce_loss).item()
                 # initialize the smoothed moving average of alpha threshold
                 if not self.alpha_thershold_smoothed:
                     self.alpha_thershold_smoothed = alpha_threshold
@@ -97,7 +96,14 @@ class DACLoss(nn.Module):
                 # loss = (1 - abstention) * (
                 #     ce_loss + torch.log(1 - abstention)
                 # ) - self.alpha * torch.log(1 - abstention)
-            return loss.mean(), ce_loss.mean(), regularization.mean(), abstention_rate
+            output = {
+                "loss": loss,
+                "CE loss": ce_loss,
+                "Regularization": regularization,
+                "Abstention": abstention,
+                "Abstention Rate": abstention_rate,
+            }
+            return output
 
 
 class IDACLoss(nn.Module):
@@ -105,20 +111,16 @@ class IDACLoss(nn.Module):
 
     def __init__(
         self,
-        noise_rate: float = 0,
-        warmup_epochs: int = 10,
+        noise_rate: float,
+        max_epochs: int,
+        warmup_rate: float = 0.2,
         alpha=None,
         **kwargs,
     ):
         super().__init__()
         self.noise_rate = noise_rate
-        self.warmup_epochs = warmup_epochs
-        if alpha is None:
-            # self.alpha = max(1, noise_rate * 50)
-            self.alpha = max(1, round(25 / np.log(noise_rate) ** 2), 2)
-        else:
-            self.alpha = alpha
-        self.epsilon = 1e-7
+        self.warmup_epochs = round(max_epochs * warmup_rate)
+        self.alpha = alpha if alpha else max(1, noise_rate * 50)
 
     def forward(
         self,
@@ -127,40 +129,39 @@ class IDACLoss(nn.Module):
         training: bool = False,
         epoch: int = 0,
     ):
-        ce_loss = F.cross_entropy(preds[:, :-1, :, :], targets, reduction="none")
+        ce_loss = F.cross_entropy(preds[:, :-1, :, :], targets)
 
         if not training:
-            return ce_loss.mean()
+            return ce_loss
         else:
-            abstention_rate = (
-                (preds.argmax(dim=1) == preds.shape[1] - 1).float().mean().item()
-            )
-            abstention = torch.exp(F.log_softmax(preds, dim=1))[:, -1, :, :]
-            abstention = torch.min(
-                abstention,
-                torch.tensor(
-                    1 - self.epsilon,
-                    device=abstention.device,
-                ),
-            )
-            regularization = torch.tensor(0.0)
+            preds = torch.exp(F.log_softmax(preds, dim=1))
+            abstention_rate = (preds.argmax(dim=1) == preds.shape[1] - 1).float().mean()
+            abstention = preds[:, -1, :, :].mean()
+            regularization = 0
 
             if epoch < self.warmup_epochs:
                 loss = ce_loss
             else:
                 ce_loss = (1 - abstention) * ce_loss
-                regularization = self.alpha * (self.noise_rate - abstention.mean()) ** 2
+                regularization = self.alpha * (self.noise_rate - abstention) ** 2
                 loss = ce_loss + regularization
-            return loss.mean(), ce_loss.mean(), regularization.mean(), abstention_rate
+            output = {
+                "loss": loss,
+                "CE loss": ce_loss,
+                "Regularization": regularization,
+                "Abstention": abstention,
+                "Abstention Rate": abstention_rate,
+            }
+            return output
 
 
 class SCELoss(nn.Module):
     """Apply Symmetric Cross Entropy Loss"""
 
-    def __init__(self, alpha=1.0, beta=0.5):
+    def __init__(self, noise_rate: float, alpha=None, A=-4):
         super().__init__()
-        self.alpha = alpha
-        self.beta = beta
+        self.alpha = alpha if alpha else 1 - noise_rate
+        self.A = A
         self.cross_entropy = nn.CrossEntropyLoss()
 
     def forward(self, pred: Tensor, targets: Tensor):
@@ -169,82 +170,153 @@ class SCELoss(nn.Module):
 
         # RCE
         pred = F.softmax(pred, dim=1)
-        pred = torch.clamp(pred, min=1e-7, max=1.0)
-        targets = torch.clamp(targets, min=1e-4, max=1.0)
-        rce = -1 * torch.sum(pred * torch.log(targets), dim=1)
+        targets = F.one_hot(targets, pred.shape[1]).float().movedim(-1, 1)
+        targets = torch.clamp(targets, min=torch.e**self.A, max=1.0)
+        rce = -torch.sum(pred * torch.log(targets), dim=1).mean()
 
-        loss = self.alpha * ce + self.beta * rce.mean()
+        loss = self.alpha * ce + rce
         return loss
 
 
-class FocalLoss(nn.Module):
-    """Apply Focal Loss"""
+class ASCELoss(nn.Module):
+    """Apply Abstaining Symmetric Cross Entropy Loss"""
 
     def __init__(
         self,
-        gamma: float = 2,
-        alpha: float = 0.25,
-        num_classes: int = 151,
+        noise_rate: float,
+        max_epochs: int,
+        warmup_epochs=None,
+        warmup_rate=None,
+        A=-2,
+        alpha=None,
+        beta=None,
         **kwargs,
     ):
         super().__init__()
-        assert gamma >= 0 and alpha >= 0 and alpha <= 1
-        self.gamma = gamma
-        self.alpha = alpha
-        self.num_classes = num_classes
+        if warmup_epochs is not None:
+            assert warmup_epochs < max_epochs
+            self.warmup_epochs = warmup_epochs
+        elif warmup_rate is not None:
+            self.warmup_epochs = round(max_epochs * warmup_rate)
+        else:
+            warmup_rate = 0.2
+            self.warmup_epochs = round(max_epochs * warmup_rate)
 
-    def forward(self, preds: Tensor, targets: Tensor) -> Tensor:
-        if targets.ndim < 4:
-            targets = F.one_hot(targets.long(), self.num_classes).movedim(-1, 1).float()
-        return sigmoid_focal_loss(
-            inputs=preds,
-            targets=targets,
-            alpha=self.alpha,
-            gamma=self.gamma,
-            reduction="mean",
-        )
-
-
-class SFLoss(nn.Module):
-    """Apply Symmetric Focal Loss"""
-
-    def __init__(
-        self,
-        alpha: float = 0.1,
-        beta: float = 1,
-        A: float = -6,
-        gamma: float = 2,
-        focal_alpha: float = 0.25,
-        num_classes: int = 151,
-        **kwargs,
-    ):
-        super().__init__()
-        assert (
-            alpha > 0
-            and beta >= 0
-            and A < 0
-            and gamma >= 0
-            and focal_alpha >= 0
-            and focal_alpha <= 1
-        )
-        self.alpha = alpha
-        self.beta = beta
+        self.noise_rate = noise_rate
         self.A = A
-        self.gamma = gamma
-        self.focal_alpha = focal_alpha
-        self.num_classes = num_classes
-        self.focal = FocalLoss(gamma=gamma, alpha=focal_alpha, num_classes=num_classes)
+        self.alpha = alpha if alpha else 1 - noise_rate
+        self.beta = beta if beta else max(1, noise_rate * 50)
 
-    def forward(self, preds: Tensor, targets: Tensor):
-        # focal
-        focal = self.focal(preds, targets)
+    def forward(
+        self,
+        preds: Tensor,
+        targets: Tensor,
+        training: bool = False,
+        epoch: int = 0,
+    ):
+        ce_loss = F.cross_entropy(preds[:, :-1, :, :], targets)
+        preds = torch.exp(F.log_softmax(preds, dim=1))
+        targets = F.one_hot(targets, preds.shape[1] - 1).float().movedim(-1, 1)
 
-        # reverse focal
-        if targets.ndim < 4:
-            targets = F.one_hot(targets.long(), self.num_classes).movedim(-1, 1).float()
-        targets = (targets - 0.5) * 2
-        preds = F.sigmoid(preds)
-        reverse_focal = self.focal(-targets * self.A, preds)
+        if not training:
+            return self.alpha * ce_loss
+        else:
+            abstention_rate = (preds.argmax(dim=1) == preds.shape[1] - 1).float().mean()
+            abstention = preds[:, -1, :, :].mean()
+            regularization = 0
+            rce = 0
 
-        loss = self.alpha * focal + self.beta * reverse_focal
-        return loss
+            if epoch < self.warmup_epochs:
+                loss = self.alpha * ce_loss
+
+            else:
+                targets = torch.clamp(targets, min=torch.e**self.A, max=1.0)
+                rce = -torch.sum(preds[:, :-1, :, :] * torch.log(targets), dim=1).mean()
+                ce_loss = (1 - abstention) * self.alpha * ce_loss
+                rce = (1 - abstention) * rce
+                regularization = self.beta * (self.noise_rate - abstention) ** 2
+                loss = ce_loss + rce + regularization
+            output = {
+                "loss": loss,
+                "CE loss": ce_loss,
+                "RCE loss": rce,
+                "Regularization": regularization,
+                "Abstention": abstention,
+                "Abstention Rate": abstention_rate,
+            }
+            return output
+
+
+# --------------------------- irrelevant ------------------------------
+
+# class FocalLoss(nn.Module):
+#     """Apply Focal Loss"""
+
+#     def __init__(
+#         self,
+#         gamma: float = 2,
+#         alpha: float = 0.25,
+#         num_classes: int = 151,
+#         **kwargs,
+#     ):
+#         super().__init__()
+#         assert gamma >= 0 and alpha >= 0 and alpha <= 1
+#         self.gamma = gamma
+#         self.alpha = alpha
+#         self.num_classes = num_classes
+
+#     def forward(self, preds: Tensor, targets: Tensor) -> Tensor:
+#         if targets.ndim < 4:
+#             targets = F.one_hot(targets.long(), self.num_classes).movedim(-1, 1).float()
+#         return sigmoid_focal_loss(
+#             inputs=preds,
+#             targets=targets,
+#             alpha=self.alpha,
+#             gamma=self.gamma,
+#             reduction="mean",
+#         )
+
+
+# class SFLoss(nn.Module):
+#     """Apply Symmetric Focal Loss"""
+
+#     def __init__(
+#         self,
+#         alpha: float = 0.1,
+#         beta: float = 1,
+#         A: float = -6,
+#         gamma: float = 2,
+#         focal_alpha: float = 0.25,
+#         num_classes: int = 151,
+#         **kwargs,
+#     ):
+#         super().__init__()
+#         assert (
+#             alpha > 0
+#             and beta >= 0
+#             and A < 0
+#             and gamma >= 0
+#             and focal_alpha >= 0
+#             and focal_alpha <= 1
+#         )
+#         self.alpha = alpha
+#         self.beta = beta
+#         self.A = A
+#         self.gamma = gamma
+#         self.focal_alpha = focal_alpha
+#         self.num_classes = num_classes
+#         self.focal = FocalLoss(gamma=gamma, alpha=focal_alpha, num_classes=num_classes)
+
+#     def forward(self, preds: Tensor, targets: Tensor):
+#         # focal
+#         focal = self.focal(preds, targets)
+
+#         # reverse focal
+#         if targets.ndim < 4:
+#             targets = F.one_hot(targets.long(), self.num_classes).movedim(-1, 1).float()
+#         targets = (targets - 0.5) * 2
+#         preds = F.sigmoid(preds)
+#         reverse_focal = self.focal(-targets * self.A, preds)
+
+#         loss = self.alpha * focal + self.beta * reverse_focal
+#         return loss
